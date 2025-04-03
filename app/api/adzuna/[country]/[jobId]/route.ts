4fc2d7c84371e6e3f8e95da05c9fa874af20ee65
Maryann-01 <amaraizummesoma@gmail.com>
2025-04-03 10:38:32 +0100

    fix:deployment build error

--- app/api/adzuna/[country]/[jobId]/route.ts
+++ app/api/adzuna/[country]/[jobId]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { country: string; jobId: string } }
+  { params }: { params: Record<string, string> }
 ) {
   const { country, jobId } = params;
   
